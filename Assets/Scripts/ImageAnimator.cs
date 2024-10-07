80b380682325250c9f2be0784028a1049e285021
jclayton128 <jclayton128@gmail.com>
2024-10-07 15:06:02 -0500

    UI polishing. Nice intro.

---
+++ Assets/Scripts/ImageAnimator.cs
@@ -0,0 +1,42 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+namespace ThinkBuildPlay.ClimateCity
+{
+
+	public class ImageAnimation : MonoBehaviour
+	{
+
+		public Sprite[] sprites;
+		public int FramesPerSprite = 6;
+		public bool IsLooping = true;
+		public bool ShouldDestroyOnEnd = false;
+
+		private int index = 0;
+		private Image image;
+		private int frame = 0;
+
+		void Awake()
+		{
+			image = GetComponent<Image>();
+		}
+
+		void Update()
+		{
+			if (!IsLooping && index == sprites.Length) return;
+			frame++;
+			if (frame < FramesPerSprite) return;
+			image.sprite = sprites[index];
+			frame = 0;
+			index++;
+			if (index >= sprites.Length)
+			{
+				if (IsLooping) index = 0;
+				if (ShouldDestroyOnEnd) Destroy(gameObject);
+			}
+		}
+	}
+
+
+}
